bff90cb0d395e096a860d6524c99b2af2fbbc271
FORMULA-12 <formula120877@gmail.com>
2024-02-11 13:06:17 -0500

    Added server script

--- src/App.jsx
+++ src/App.jsx
@@ -55,7 +55,7 @@ function App() {
                       <div className='icon'><i class="fa-solid fa-chalkboard-user"></i></div>
                       <div className='title-subtitle d-inline-flex flex-column'>
                         <p className='title'>Руководства и инструкции</p>
-                        <p className='subtitle'>Вы ищете партнеров? Создаете сервер? Хотите совместно работать над проектом? В данном разделе вы можете собрать команду.</p>
+                        <p className='subtitle'>Полезная информация , которая поможет повысить эффективность работы и успешно справиться с поставленными задачами.</p>
                       </div>
                     </div>
                     <div className='open'><i class="fa-solid fa-up-right-from-square"></i></div>
@@ -68,7 +68,46 @@ function App() {
                       <div className='icon'><i class="fa-solid fa-people-robbery"></i></div>
                       <div className='title-subtitle d-inline-flex flex-column'>
                         <p className='title'>Инициативы и предложения</p>
-                        <p className='subtitle'>Вы ищете партнеров? Создаете сервер? Хотите совместно работать над проектом? В данном разделе вы можете собрать команду.</p>
+                        <p className='subtitle'>Делитесь своими идеями, обменивайтесь мнениями с коммьюнити и получайте обратную связь от экспертов.</p>
+                      </div>
+                    </div>
+                    <div className='open'><i class="fa-solid fa-up-right-from-square"></i></div>
+                  </div>
+                </div>
+
+                <div className='topic'>
+                  <div className='d-flex justify-content-between'>
+                    <div>
+                      <div className='icon'><i class="fa-solid fa-cubes"></i></div>
+                      <div className='title-subtitle d-inline-flex flex-column'>
+                        <p className='title'>Кубическое творчество</p>
+                        <p className='subtitle'>Делитесь своими идеями, обменивайтесь мнениями с коммьюнити и получайте обратную связь от экспертов.</p>
+                      </div>
+                    </div>
+                    <div className='open'><i class="fa-solid fa-up-right-from-square"></i></div>
+                  </div>
+                </div>
+
+                <div className='topic'>
+                  <div className='d-flex justify-content-between'>
+                    <div>
+                      <div className='icon'><i class="fa-solid fa-swords"></i></div>
+                      <div className='title-subtitle d-inline-flex flex-column'>
+                        <p className='title'>Проведение турниров</p>
+                        <p className='subtitle'>Делитесь своими идеями, обменивайтесь мнениями с коммьюнити и получайте обратную связь от экспертов.</p>
+                      </div>
+                    </div>
+                    <div className='open'><i class="fa-solid fa-up-right-from-square"></i></div>
+                  </div>
+                </div>
+
+                <div className='topic end'>
+                  <div className='d-flex justify-content-between'>
+                    <div>
+                      <div className='icon'><i class="fa-solid fa-messages-question"></i></div>
+                      <div className='title-subtitle d-inline-flex flex-column'>
+                        <p className='title'>Другие темы</p>
+                        <p className='subtitle'>Делитесь своими идеями, обменивайтесь мнениями с коммьюнити и получайте обратную связь от экспертов.</p>
                       </div>
                     </div>
                     <div className='open'><i class="fa-solid fa-up-right-from-square"></i></div>
@@ -79,6 +118,22 @@ function App() {
           </div>
         </div>
       </div>
+
+      <footer>
+        <div className="container">
+          <div className='div-link d-flex justify-content-between'>
+            <p className='link'><a>Услуги</a></p>
+            <p className='link'><a>Информация</a></p>
+            <p className='link'><a>Помощь</a></p>
+          </div>
+
+          <div className='d-flex justify-content-between'>
+            <p className='link'><a>Главная</a></p>
+            <p className='link'><a>Главная</a></p>
+            <p className='link'><a>Главная</a></p>
+          </div>
+        </div>
+      </footer>
     </>
   )
 }
